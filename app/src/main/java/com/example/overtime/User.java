79619bc5a69ec875a8cf1726e988fcafd3ac1b9f
james57757 <jamespantigo@gmail.com>
2021-11-18 22:53:10 -0800

    Initial commit

---
+++ app/src/main/java/com/example/overtime/User.java
@@ -0,0 +1,10 @@
+package com.example.overtime;
+
+import com.google.android.gms.maps.GoogleMap;
+
+import java.util.Map;
+
+public class User {
+    public String timezone;
+    public GoogleMap location;
+}
